2fe48985d248a8e5684248cfef6d56aabca07b6f
Eric Thomas <ethomasosu@gmail.com>
2024-12-01 17:43:04 -0500

    add day 1 solutions

---
+++ day1/day1-1.py
@@ -0,0 +1,16 @@
+with open("day1-data.txt", "r") as fp:
+    list_1, list_2 = [], []
+    for line in fp:
+        distance1, distance2 = line.split()
+        list_1.append(int(distance1))
+        list_2.append(int(distance2))
+
+list_1.sort()
+list_2.sort()
+
+distance_difference = 0
+
+for i in range(len(list_1)):
+    distance_difference += abs(list_1[i] - list_2[i])
+
+print(f"Distance difference - {distance_difference}")
---
+++ day1/day1-2.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+with open("day1-data.txt", "r") as fp:
+    count = defaultdict(int)
+    left = []
+    for line in fp:
+        left_val, right_val = line.split()
+        left.append(left_val)
+        count[right_val] += 1
+
+similarity = 0
+for val in left:
+    similarity += int(val) * count[val]
+
+print(f"Similarity score - {similarity}")
