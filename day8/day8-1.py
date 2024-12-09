dfad5245d91fccad58c4acce5e07c14749758b27
Eric Thomas <ethomasosu@gmail.com>
2024-12-09 09:10:40 -0500

    add day8 solutions

---
+++ day8/day8-1.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+
+grid = []
+with open("inputs/day8-data.txt", "r") as fp:
+    for line in fp:
+        grid.append(list(line.strip()))
+
+
+ROWS = len(grid)
+COLS = len(grid[0])
+
+antenna_to_pos = defaultdict(list)
+for row in range(ROWS):
+    for column in range(COLS):
+        if grid[row][column] != ".":
+            antenna_to_pos[grid[row][column]].append((row, column))
+
+
+antinodes = set()
+for antenna, positions in antenna_to_pos.items():
+    for i in range(len(positions)):
+        for j in range(len(positions)):
+            if i == j:
+                continue
+            p1_row, p1_column = positions[i]
+            p2_row, p2_column = positions[j]
+
+            row_diff = p1_row - p2_row
+            column_diff = p1_column - p2_column
+
+            antinode_row = p1_row + row_diff
+            antinode_column = p1_column + column_diff
+            if (
+                0 <= antinode_row < ROWS
+                and 0 <= antinode_column < COLS
+                and grid[antinode_row][antinode_column] != grid[p1_row][p1_column]
+            ):
+                antinodes.add((antinode_row, antinode_column))
+
+print(f"Antinodes {len(antinodes)}")
