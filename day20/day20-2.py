0334e6ebdf098016565836ad133703cb820df876
Eric Thomas <ethomasosu@gmail.com>
2024-12-20 11:25:23 -0500

    add day 20 part 2 solution

--- day20/day20-2.py
+++ day20/day20-2.py
@@ -21,15 +21,13 @@ SOUTH = (1, 0)
 WEST = (0, -1)
 DIRECTIONS = [NORTH, EAST, SOUTH, WEST]
 
-queue = collections.deque([[start, set([start]), 0]])
+queue = collections.deque([start])
 visited = set([start])
-positions_to_step_count = {start: 0}
-best_path = None
+tiles_walked = [start]
 while len(queue) > 0:
-    pos, prev_posisitions, step = queue.popleft()
+    pos = queue.popleft()
     row, column = pos
     if grid[row][column] == "E":
-        best_path = prev_posisitions
         break
 
     for direction in DIRECTIONS:
@@ -42,58 +40,28 @@ while len(queue) > 0:
             and grid[new_row][new_column] in [".", "E"]
             and (new_row, new_column) not in visited
         ):
-            prev_positions_copy = copy.deepcopy(prev_posisitions)
-            prev_positions_copy.add((new_row, new_column))
             visited.add((new_row, new_column))
-            positions_to_step_count[(new_row, new_column)] = step + 1
-            queue.append([(new_row, new_column), prev_positions_copy, step + 1])
+            tiles_walked.append((new_row, new_column))
+            queue.append((new_row, new_column))
 
-PICOSECONDS_SAVED_THRESHOLD = 75
+PICOSECONDS_SAVED_THRESHOLD = 100
 PICSOSECONDS_CHEAT_THRESHOLD = 20
 
 
-def bfs_cheat(start, positions_to_step_count):
-    queue = collections.deque([start])
-    visited = set([start])
-    cheat_start_and_end_to_picoseconds_saved = {}
-    steps = 1
-    while len(queue) > 0 and steps < PICSOSECONDS_CHEAT_THRESHOLD:
-        queue_len = len(queue)
-        for _ in range(queue_len):
-            row, column = queue.popleft()
-            for row_change, column_change in DIRECTIONS:
-                new_row = row + row_change
-                new_column = column + column_change
-                if 0 <= new_row < ROWS and 0 <= new_column < COLUMNS and (new_row, new_column) not in visited:
-                    # Don't add . to queue bc cheats should only go through walls until last step of a .
-                    if grid[new_row][new_column] == "#":
-                        visited.add((new_row, new_column))
-                        queue.append((new_row, new_column))
-                    # We've found a . ending our cheat so compare it and see if we saved anything
-                    elif (
-                        steps >= 2
-                        and (new_row, new_column) in positions_to_step_count
-                        and positions_to_step_count[(new_row, new_column)] - positions_to_step_count[start] > steps
-                    ):
-                        cheat_start_and_end_to_picoseconds_saved[(start, (new_row, new_column))] = (
-                            positions_to_step_count[(new_row, new_column)]
-                        )
-                        -positions_to_step_count[start]
-        steps += 1
-
-    return cheat_start_and_end_to_picoseconds_saved
+def get_manhattan_distance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 
-total_steps = len(positions_to_step_count)
-cheat_start_and_end_to_picoseconds_saved = {}
-for pos in positions_to_step_count:
-    cheat_start_and_end_to_picoseconds_saved.update(bfs_cheat(pos, positions_to_step_count))
+cheats_to_picoseconds_saved_over_threshold = {}
+for step, pos in enumerate(tiles_walked):
+    for step2 in range(step + 1, len(tiles_walked)):
+        pos2 = tiles_walked[step2]
+        cheat_distance = get_manhattan_distance(pos, pos2)
+        if (
+            pos != pos2
+            and cheat_distance <= PICSOSECONDS_CHEAT_THRESHOLD
+            and step2 - step - cheat_distance >= PICOSECONDS_SAVED_THRESHOLD
+        ):
+            cheats_to_picoseconds_saved_over_threshold[(pos, pos2)] = step2 - step - cheat_distance
 
-cheats_over_threshold = 0
-for picoseconds_saved in cheat_start_and_end_to_picoseconds_saved.values():
-    if picoseconds_saved > PICOSECONDS_SAVED_THRESHOLD:
-        cheats_over_threshold += 1
-
-print(cheat_start_and_end_to_picoseconds_saved)
-print(cheats_over_threshold)
-print(positions_to_step_count)
+print(len(cheats_to_picoseconds_saved_over_threshold))
